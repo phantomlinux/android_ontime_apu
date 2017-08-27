166268b348d4e99f3db8c1bae0e6a5000ecf460c
phantomlinux <vaughnchong@gmail.com>
2017-08-28 03:09:42 +0800

    wip

---
+++ app/src/main/java/com/example/phantomlinux/ontime/TimetableModel.java
@@ -0,0 +1,8 @@
+package com.example.phantomlinux.ontime;
+
+/**
+ * Created by phantomlinux on 01/06/2017.
+ */
+
+public class TimetableModel {
+}
---
+++ app/src/main/java/com/example/phantomlinux/ontime/WeekDialogFragment.java
@@ -0,0 +1,14 @@
+package com.example.phantomlinux.ontime.Util;
+
+import android.app.DialogFragment;
+
+/**
+ * Created by phantomlinux on 26/08/2017.
+ */
+
+public class WeekDialogFragment extends DialogFragment {
+
+
+
+
+}
